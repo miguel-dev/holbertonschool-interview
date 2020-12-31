44fd883e740b6e052a386557846eb3abc727b34d
miguel-dev <miguelacortes@zoho.com>
2020-12-31 00:47:48 +0000

    [Fixes] Added a recursive insert function to insert the node

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -2,6 +2,40 @@
 #include <stdlib.h>
 
 /**
+ * heap_insert2 - Inserts a value into a Max Binary Heap.
+ *
+ * @root: Address to root node of Heap.
+ * @new_node: Node to be inserted.
+ *
+ * Return: Pointer to the inserted node, or NULL if it fails.
+ */
+heap_t *heap_insert2(heap_t *root, heap_t *new_node)
+{
+	if (root->left == NULL)
+	{
+		root->left = new_node;
+		new_node->parent = root;
+	}
+	else if (new_node->right == NULL)
+	{
+		root->right = new_node;
+		new_node->parent = root;
+	}
+	else
+	{
+		new_node = heap_insert2(root->left, new_node);
+		if (new_node)
+		{
+			return (new_node);
+		}
+
+		new_node = heap_insert2(root->right, new_node);
+	}
+	return (new_node);
+}
+
+
+/**
  * heap_insert - Inserts a value into a Max Binary Heap.
  *
  * @root: Address to root node of Heap.
@@ -11,9 +45,9 @@
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	binary_tree_t *new_node;
+	heap_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(heap_t));
 	if (!new_node)
 	{
 		return (NULL);
@@ -27,6 +61,10 @@ heap_t *heap_insert(heap_t **root, int value)
 	if (*root == NULL)
 	{
 		*root = new_node;
+	}
+	else
+	{
+		new_node = heap_insert2(*root, new_node);
 	}
 
 	return (new_node);
