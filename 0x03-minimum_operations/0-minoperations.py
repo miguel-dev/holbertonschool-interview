7b07d06b3547402674a484e3503a473c3ea31025
miguel-dev <miguelacortes@zoho.com>
2021-01-05 03:41:01 +0000

    [Fixes] Cleaned the comment

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def minOperations(n):
-    if (n <= 1):   # l do nothing, 0 or negative nothing can be done*/
+    if (n <= 1):   # l do nothing, 0 or negative nothing can be done
         return 0
     
     half = int(n / 2)
