250665e031b78039fea20ac0e458aa1353f41264
miguel-dev <miguelacortes@zoho.com>
2021-01-27 00:14:08 +0000

    0. Log parsing

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import sys
+
+total_size = 0
+status_codes = {}
+num = 0
+
+try:
+    for line in sys.stdin:
+        parsed = line.split()
+        total_size += int(parsed[-1]);
+        code = parsed[-2]
+
+        if code in status_codes:
+            status_codes[code] += 1
+        else:
+            status_codes[code] = 1
+
+        if (num % 10 == 0):
+            print("File size: {:d}".format(total_size))
+
+            for c in sorted(status_codes):
+                print("{:s}: {:d}".format(c, status_codes[c]))
+
+        num += 1
+
+except KeyboardInterrupt:
+    print("File size: {:d}".format(total_size))
+
+    for c in sorted(status_codes):
+        print("{:s}: {:d}".format(c, status_codes[c]))
