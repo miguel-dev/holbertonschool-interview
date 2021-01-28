8445175503e811c5e46ef35deaee2e5149251627
miguel-dev <miguelacortes@zoho.com>
2021-01-28 17:15:50 +0000

    [Fixes] Uses a try/except clause in the case the int conversion of size of status code fails

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -17,24 +17,28 @@ if __name__ == "__main__":
             if len(parsed) != 9:
                 continue
 
-            total_size += int(parsed[-1])
-            code = int(parsed[-2])
+            try: 
+                total_size += int(parsed[-1])
+                code = int(parsed[-2])
+            except:
+                continue
 
             if code not in valid_codes:
                 continue
+            
+            num += 1
 
             if code in status_codes:
                 status_codes[code] += 1
             else:
                 status_codes[code] = 1
 
-            num += 1
-
             if (num % 10 == 0):
                 print("File size: {:d}".format(total_size))
 
                 for c in sorted(status_codes):
                     print("{:d}: {:d}".format(c, status_codes[c]))
+            
 
     except KeyboardInterrupt:
         print("File size: {:d}".format(total_size))
