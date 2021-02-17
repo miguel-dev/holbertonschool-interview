4ea86a8f2c8cfcd74b75fc860957116d5c10c11f
miguel-dev <miguelacortes@zoho.com>
2021-02-17 21:05:21 +0000

    [Fixes] Checks the headers of each byte

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,6 +5,23 @@
 
 def validUTF8(data):
     for i in data:
+        char_left = 0
         if i > 128:
             return False
+        else:
+            binary = str(bin(i))
+            binary = binary[-8:]
+
+            if (char_left > 0):
+                if (binary[0] == 1 and binary[1] == 0):
+                    continue
+                else:
+                    return False
+
+            if binary.find("110") == 0:
+                char_left = 1
+            elif binary.find("1110") == 0:
+                char_left = 2
+            elif binary.find("11110") == 0:
+                char_left = 3
     return True
