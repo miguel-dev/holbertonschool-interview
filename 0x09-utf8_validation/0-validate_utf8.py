9773b61c2136d099bd3122e134989f9e10c5b399
miguel-dev <miguelacortes@zoho.com>
2021-02-18 04:02:45 +0000

    [Fixes] Checks if integer sent is 0 and fixes other things

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -8,8 +8,7 @@ def validUTF8(data):
     byte_left = 0
 
     for i in data:
-        binary = str('{0: 8b}'.format(i & 255))
-        binary = binary[1:]
+        binary = str('{0:08b}'.format(i & 255))
 
         if (byte_left > 0):
             if len(binary) == 8 and binary[0] == "1" and binary[1] == "0":
@@ -19,12 +18,16 @@ def validUTF8(data):
                 return False
 
         if len(binary) == 8:
-            if binary.find("110") == 0:
+            if binary[:3] == "110":
                 byte_left = 1
-            elif binary.find("1110") == 0:
+            elif binary[:4] == "1110":
                 byte_left = 2
-            elif binary.find("11110") == 0:
+            elif binary[:5] == "11110":
                 byte_left = 3
+            elif binary[:2] == "01":
+                pass
+            elif binary == "00000000":
+                pass
             else:
                 return False
     return True
