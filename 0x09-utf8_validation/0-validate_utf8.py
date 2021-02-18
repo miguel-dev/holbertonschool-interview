107afde81c6228a44816b5861b7d27a39ab58ab1
miguel-dev <miguelacortes@zoho.com>
2021-02-18 02:21:48 +0000

    [Fixes] Eliminated the checks for integers

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,29 +4,27 @@
 
 
 def validUTF8(data):
+    """Checks for valid UTF-8 encoding"""
     byte_left = 0
 
     for i in data:
-        if i > 244 or i == 192 or i == 193:
-            return False
-        else:
-            binary = str('{0: 08b}'.format(i))
-            binary = binary[1:]
+        binary = str('{0: 08b}'.format(i))
+        binary = binary[1:]
 
-            if (byte_left > 0):
-                if len(binary) == 8 and binary[0] == "1" and binary[1] == "0":
-                    byte_left -= 1
-                    continue
-                else:
-                    return False
+        if (byte_left > 0):
+            if len(binary) == 8 and binary[0] == "1" and binary[1] == "0":
+                byte_left -= 1
+                continue
+            else:
+                return False
 
-            if (len(binary) == 8):
-                if binary.find("110") == 0:
-                    byte_left = 1
-                elif binary.find("1110") == 0:
-                    byte_left = 2
-                elif binary.find("11110") == 0:
-                    byte_left = 3
-                else:
-                    return False
+        if len(binary) == 8:
+            if binary.find("110") == 0:
+                byte_left = 1
+            elif binary.find("1110") == 0:
+                byte_left = 2
+            elif binary.find("11110") == 0:
+                byte_left = 3
+            else:
+                return False
     return True
